5dec03956ff1f25c1f671f7660359160b7fa37e2
DaniRuiz1983 <daniruiz.digital@gmail.com>
2025-07-04 17:08:34 -0300

    Primer commit

---
+++ DaniRuiz1983.py
@@ -0,0 +1,20 @@
+ 
+#https://www.python.org/
+#comemtario de prueba
+
+"""
+esto tambien es un comentarios
+
+"""
+mi_variable = "hola mundo"
+MI_CONSTANTE = 3.1406 # es una constante por convencion cuando se escribe con mayuscula
+
+#4 datos primitivos
+my_int = 10
+my_float = 10.2
+my_bool = True
+my_bool = False
+my_string = "Daniela"
+my_other_string = 'Dani'
+
+print("Hola, Pithon")
